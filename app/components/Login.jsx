64a3224f5a9a0fb296c5e8035fd6f69fa2f7b72f
KemCake <santos.remi@icloud.com>
2015-10-23 11:20:37 +0200

    Add Login Style

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,20 +1,35 @@
-import { Reapp, View, Button, List, Input } from 'reapp-kit';
+import { Reapp, View, List, Input } from 'reapp-kit';
+
+const styles = {
+  container: {
+    background: '-webkit-linear-gradient(90deg, #ffd194 10%, #70e1f5  90%)',
+  },
+  form: {
+    marginTop: 0,
+  },
+  icon: {
+    fontSize: 100,
+    textAlign: 'center',
+  },
+};
 
 class Login extends React.Component {
   render() {
     return (
       <View>
-        <form>
-          <p>Login</p>
-          <List wrap>
-            <Input type="text" placeholder="Username" />
-            <Input type="text" placeholder="Password"/>
-          </List>
-          <br/>
-          <Button onTap={this.login.bind(this)}>
-            Login
-          </Button>
-        </form>
+        <div className="fullscreen" style={styles.container}>
+          <h1 style={styles.icon}>👯</h1>
+          <form style={styles.form}>
+            <List wrap>
+              <Input type="text" placeholder="Username" />
+              <Input type="text" placeholder="Password"/>
+            </List>
+            <br/>
+            <List>
+              <List.Item icon={true} title="Login" onTap={this.login.bind(this)} />
+            </List>
+          </form>
+        </div>
       </View>
     );
   }
@@ -23,5 +38,6 @@ class Login extends React.Component {
     this.router().transitionTo('home');
   }
 }
+
 
 export default Reapp(Login);
