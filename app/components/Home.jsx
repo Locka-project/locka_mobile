b01aef08786992400732cdc674557b6eb56dbaaf
KemCake <santos.remi@icloud.com>
2015-10-30 14:39:44 +0100

    Add tab switching

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,32 +1,57 @@
 import { Reapp, View, Bar } from 'reapp-kit';
 import AuthRequired from '../mixins/AuthRequired';
-import Devices from './Devices';
-// import Settings from './Settings';
+
+const styles = {
+  iconStyles: {
+    width: 25,
+    height: 25,
+    marginTop: 5,
+  },
+};
+
+const routes = ['devices', 'settings'];
 
 class Home extends React.Component {
 
   constructor() {
     super();
     this.state = {
-      active: 'DEVICES', // || 'SETTINGS' || ...
+      activeTab: 0,
     };
   }
-  render() {
 
+  render() {
+    const BarItem = Bar.Item;
     return (
       <View
         offsetBottom={49}
         disableScroll={true}
         after={
-          <Bar {...this.props} title="Home">
-            <Bar.Item icon="home">Devices</Bar.Item>
-            <Bar.Item icon="person">Settings</Bar.Item>
+          <Bar>
+            <BarItem active={this.state.activeTab === 0}
+              icon={require('reapp-kit/icons/key.svg')}
+              iconProps={{style: styles.iconStyles}}
+              onTap={() => this.changeTab(0)}>
+              Devices
+            </BarItem>
+            <BarItem active={this.state.activeTab === 1}
+              icon={require('reapp-kit/icons/phone.svg')}
+              iconProps={{style: styles.iconStyles}}
+              onTap={() => this.changeTab(1)}>
+              Settings
+            </BarItem>
           </Bar>
       }>
         {this.props.child()}
       </View>
     );
   }
+
+  changeTab(tabIndex) {
+    this.setState({activeTab: tabIndex});
+    this.router().transitionTo(routes[tabIndex]);
+  }
+
 }
 
 export default AuthRequired(Reapp(Home));
