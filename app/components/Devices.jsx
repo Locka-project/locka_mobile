1223029078c4acbd83ba6454ec0264898df8fdd3
KemCake <santos.remi@icloud.com>
2015-11-05 11:03:36 +0100

    Fix overflow

--- app/components/Devices.jsx
+++ app/components/Devices.jsx
@@ -8,7 +8,7 @@ class Devices extends React.Component {
     var addButton = <Button chromeless onTap={this.showNewDevicePage.bind(this)}>Add</Button>;
     return (
       <div>
-          <View title={['', 'Devices', addButton]}>
+          <View style={{overflow: 'hidden'}} title={['', 'Devices', addButton]}>
             <DevicesList />
           </View>
           {this.props.child()}
